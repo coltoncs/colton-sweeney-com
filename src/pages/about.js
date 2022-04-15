f14b5cd06eb7130a070af5ca82e7f616d4e845d9
BuildTools <unconfigured@null.spigotmc.org>
2022-04-15 12:56:50 -0400

    create about page and reset content

---
+++ src/pages/about.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Helmet from "react-helmet";
+import Layout from "../components/layout";
+import { Link } from "gatsby";
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <Helmet>
+        <title>About</title>
+      </Helmet>
+      <div className="headline">About</div>
+      <div>
+        <p>
+          This is my personal, yet minimalists portfolio/blog website that I
+          will be building upon and adding content to in my free time. Right
+          now, this site is still in development, so please bare with me as I
+          update and progress!
+        </p>
+        <p>
+          Feel free to check out my <Link to="https://github.com/coltoncs">Github</Link> for my current and past projects, and keep an eye on my <Link to="https://www.linkedin.com/in/colton-sweeney/">LinkedIn</Link> for more information on me!
+        </p>
+      </div>
+    </Layout>
+  );
+};
+
+export default AboutPage;
