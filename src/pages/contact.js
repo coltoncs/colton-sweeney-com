bef576a7004bbaa033b087ea1dc11395e84f1323
BuildTools <unconfigured@null.spigotmc.org>
2022-04-17 12:27:41 -0400

    add netlify contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,22 +20,23 @@ const ContactPage = ({
           <p>Let me help you kick start your next project &rarr;</p>
         </div>
         <div>
-          <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
+          <form className="form-container" data-netlify="true" data-netlify-honeypot="bot-field" method="POST" name="Contact">
+            <input type="hidden" name="form-name" value="Contact" />
             <div>
-              <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName"/>
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="w3lName"/>
             </div>
             <div>
-              <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender"/>
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="w3lSender"/>
             </div>
             <div>
-              <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject"/>
+              <label htmlFor="subject">Subject</label>
+              <input type="text" name="subject" id="w3lSubject"/>
             </div>
             <div>
-              <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
+              <label htmlFor="message">Message</label>
+              <textarea name="message" id="w3lMessage"></textarea>
             </div>
             <div style={{display: "flex", justifyContent: "flex-end"}}>
               <input type="submit" className="button -primary" style={{marginRight: 0}} />
